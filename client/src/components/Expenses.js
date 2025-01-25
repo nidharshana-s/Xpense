ddeafff13b0572c0a2e1ec1a744f131a1f3b69eb
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-26 00:17:42 +0530

    Expenses and its form is over

--- client/src/components/Expenses.js
+++ client/src/components/Expenses.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import styled from 'styled-components'
 import { InnerLayout } from '../styles/Layouts'
+import Form from './Form';
+import IncomeItem from './IncomeItem';
+import ExpenseForm from './ExpenseForm';
+import { useGlobalContext } from '../context/globalContext'; // Importing the context
 
 const Expenses = () => {
+  const { totalExpenses, expenses, deleteExpense } = useGlobalContext(); // Using the context
+
   return (
     <ExpensesStyled>
         <InnerLayout>
-                    
+                <h1>Expenses</h1>
+                <h2 className="total-income">Total Expense: <span>${totalExpenses()}</span></h2>
+                <div className="income-content">
+                    <div className="form-container">
+                        <ExpenseForm />
+                    </div>
+                    <div className="incomes">
+                        {expenses.map((income) => {
+                            const {_id, title, amount, date, category, description, type} = income;
+                            return <IncomeItem
+                                key={_id}
+                                id={_id} 
+                                title={title} 
+                                description={description} 
+                                amount={amount} 
+                                date={date} 
+                                type={type}
+                                category={category} 
+                                indicatorColor="var(--color-green)"
+                                deleteItem={deleteExpense}
+                            />
+                        })}
+                    </div>
+                </div>
         </InnerLayout>
     </ExpensesStyled>
   )
