73fdd06d0a60179f48fde95106f57307897cfeb1
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-25 22:56:52 +0530

    Navigation bar pushed

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,17 @@ import styled from "styled-components";
 import { MainLayout } from "./styles/Layouts";
 import bg from './img/bg.png'
 import Orb from "./components/Orb";
+import Navigation from "./components/Navigation";
+import { useState } from "react";
+
 
 function App() {
+  const [active, setActive] = useState(1);
   return (
     <AppStyled classname="App">
       <Orb />
       <MainLayout>
-
+        <Navigation active={active} setActive={setActive}/>
       </MainLayout>  
     </AppStyled>
   );
