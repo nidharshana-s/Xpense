87a3b740b4550ef9041e5d4ab7410f9f515b871d
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-25 22:01:55 +0530

    Backgroud images and design over

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
+import styled from "styled-components";
+import { MainLayout } from "./styles/Layouts";
+import bg from './img/bg.png'
+import Orb from "./components/Orb";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled classname="App">
+      <Orb />
+      <MainLayout>
+
+      </MainLayout>  
+    </AppStyled>
   );
 }
+
+const AppStyled = styled.div`
+    height: 100vh;
+    background-image: url(${bg});
+    position: relative;
+  `
 
 export default App;
---
+++ client/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from "react"; 
+import styled from "styled-components";
+
+function Navigation() {
+    return (
+        <NavStyle>
+            
+        </NavStyle>     
+    )
+}
+
+const NavStyle = styled.div`
+
+`;
+
+export default Navigation;
