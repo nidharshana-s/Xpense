1e46843b413e8adee8ec5f9919d002b212ce963c
Nidharshana S <nidharshana0402@gmail.com>
2025-01-25 21:41:59 +0530

    income and expense added

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors');
 const { db } = require('./db/db');
-// const {readdirSync} = require('fs')
+const {readdirSync} = require('fs')
 const app = express()
 
 require('dotenv').config()
@@ -12,9 +12,9 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
-app.get('/',(req, res) => {
-    res.send("hello")
-})
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
 //routes
 //readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
 
