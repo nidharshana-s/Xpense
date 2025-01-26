47e3caa807e41dd714b4f98212fb4d62fc043f29
SanjayS05 <sanjaysankar2nd@gmail.com>
2025-01-26 09:40:34 +0530

    Revert "Working fine"
    This reverts commit a603e4b7b4e7e4b777252483e732974b342a1a66.

--- client/src/components/Income.js
+++ client/src/components/Income.js
@@ -1,43 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import styled from 'styled-components'
-import { useGlobalContext } from '../context/globalContext';
-import { InnerLayout } from '../styles/Layouts';
-import Form from './Form';
-import IncomeItem from './IncomeItem';
+import { InnerLayout } from '../styles/Layouts'
+
 const Income = () => {
-  const {addIncome, incomes, getIncomes, deleteIncome, totalIncome} = useGlobalContext()
-
-    useEffect(() =>{
-        getIncomes()
-    }, [])
-
   return (
     <IncomeStyled>
         <InnerLayout>
-        <h1>Incomes</h1>
-                <h2 className="total-income">Total Income: <span>${totalIncome()}</span></h2>
-                <div className="income-content">
-                    <div className="form-container">
-                        <Form />
-                    </div>
-                    <div className="incomes">
-                        {incomes.map((income) => {
-                            const {_id, title, amount, date, category, description, type} = income;
-                            return <IncomeItem
-                                key={_id}
-                                id={_id} 
-                                title={title} 
-                                description={description} 
-                                amount={amount} 
-                                date={date} 
-                                type={type}
-                                category={category} 
-                                indicatorColor="var(--color-green)"
-                                deleteItem={deleteIncome}
-                            />
-                        })}
+            <h1>Incomes</h1>
+            <div className='income-content'>
+                <div className='form-container'>
+                    <div className='incomes'>
+
                     </div>
                 </div>
+            </div>
         </InnerLayout>
     </IncomeStyled>
   )
